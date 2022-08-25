7e1e4f2de7532d3ed6c52778205bd3125ce9ed22
Laisses <71999249+Laisses@users.noreply.github.com>
2022-08-25 08:08:03 -0300

    adiciona função que pega mensagens no servidor

--- script.js
+++ script.js
@@ -17,3 +17,63 @@ const joinChatRoom = () => {
             }
         });
 };
+
+const getsMessages = () => {
+    const URL = "https://mock-api.driven.com.br/api/v6/uol/messages";
+    axios.get(URL)
+        .then(res => {
+            createMessages(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+};
+
+const createMessages = messages => {
+    const ul = document.querySelector(".msg");
+
+    messages.forEach(message => {
+        const messageType = document.createElement("li");
+        
+        const timeWrapper = document.createElement("div");
+        const timeStamp = document.createElement("p");
+
+        const textWrapper = document.createElement("div");
+        const text = document.createElement("p");
+        
+        if (message.type === "private_message") {
+            messageType.classList = "msg__private";
+
+            textWrapper.innerHTML = `
+                <p><span>${message.from}</span> reservadamente para ${message.to}: ${message.text}</p>
+            `;
+        } else if (message.type === "message") {
+            messageType.classList = "msg__public";
+
+            textWrapper.innerHTML = `
+            <p><span>${message.from}</span> para <span>${message.to}</span>: ${message.text}</p>
+            `;
+        } else {
+            messageType.classList = "msg__notification";
+
+            textWrapper.innerHTML = `
+                <p><span>${message.from}</span> ${message.text}</p>
+            `;
+        }
+
+        timeWrapper.classList = "msg__time";
+        timeStamp.textContent = message.time;
+
+        textWrapper.classList = "msg__chat";       
+        
+        textWrapper.appendChild(text);   
+        timeWrapper.appendChild(timeStamp);
+        messageType.appendChild(timeWrapper);
+        messageType.appendChild(textWrapper);
+        ul.appendChild(messageType);
+    });
+};
+
+
+
